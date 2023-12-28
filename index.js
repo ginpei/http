49ecc5343f0e297e1bdf354f19730665af5bf9bb
Ginpei <ginpei@ginpei.dev>
2023-12-27 16:12:45 -0800

    live reload over ws

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 const express = require('express');
+const expressWs = require('express-ws');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
+const filePath = '.';
 
 const app = express();
-app.use(express.static('.')); // working dir
+expressWs(app);
+
+app.ws('/liveReload', function(ws) {
+  fs.watch(filePath, () => {
+    ws.send('changed');
+  });
+});
+
+app.use(express.static(filePath)); // working dir
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
 });
